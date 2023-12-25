726da103d09c8e173125ed4c3a454f5bff7bf459
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-12-25 14:42:04 +0530

    fullyRefactored signup_screen

--- lib/screens/signup_screen/signup_screen.dart
+++ lib/screens/signup_screen/signup_screen.dart
@@ -1,10 +1,69 @@
 import 'package:flutter/material.dart';
 
+import '../../theme/responsive_theme.dart';
+import '../../theme/styled_theme.dart';
+import '../widget/background.dart';
+import 'widget/sign_up_top_image.dart';
+import 'widget/signup_form.dart';
+
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Sign Up");
+    return const Background(
+      child: SingleChildScrollView(
+        child: Responsive(
+          mobile: MobileSignupScreen(),
+          desktop: Row(
+            children: [
+              Expanded(
+                child: SignUpScreenTopImage(),
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 450,
+                      child: SignUpForm(),
+                    ),
+                    SizedBox(height: AppSizes.defaultPadding / 2),
+                    // SocalSignUp()
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MobileSignupScreen extends StatelessWidget {
+  const MobileSignupScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        SignUpScreenTopImage(),
+        Row(
+          children: [
+            Spacer(),
+            Expanded(
+              flex: 8,
+              child: SignUpForm(),
+            ),
+            Spacer(),
+          ],
+        ),
+        // const SocalSignUp()
+      ],
+    );
   }
 }
